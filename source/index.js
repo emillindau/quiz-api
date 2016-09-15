f9b83e7c59d8e0b844f885db0edf1a51d4276d13
Emil Lindau <emil.lindau@schibsted.se>
2016-09-15 16:28:14 +0200

    delete and update

--- source/index.js
+++ source/index.js
@@ -22,40 +22,71 @@
 
     // Middlewares
     router.use(function(req, res, next) {
-        console.log('Called endpoint');
-        next();
+      console.log('Called endpoint');
+      next();
     });
 
     router.get('/', (req, res) => {
-        res.json({message: 'Welcome to quz api!'});
+      res.json({message: 'Welcome to quz api!'});
     });
 
     router.route('/questions')
     .post(function(req, res) {
-        let question = new Question();
-        question.answer = req.body.answer;
-        question.value = req.body.value;
-        question.question = req.body.question;
-        question.categoryName = req.body.categoryName;
+      let question = new Question();
+      question.answer = req.body.answer;
+      question.value = req.body.value;
+      question.question = req.body.question;
+      question.categoryName = req.body.categoryName;
 
-        question.save(function(err) {
-            if(err) {
-                res.send(err);
-            }
-            res.json({message: 'Question created!'});
-        });
+      question.save(function(err) {
+        if(err) {
+          res.send(err);
+        }
+        res.json({message: 'Question created!'});
+      });
     })
     .get(function(req, res) {
-        Question.find(function(err, qs) {
-            if(err) {
-                res.send(err);
-            }
-            res.json(qs);
-        });
+      Question.find(function(err, qs) {
+        if(err) {
+          res.send(err);
+        }
+        res.json(qs);
+      });
+    });
+
+    router.route('/questions/delete')
+    .post(function(req, res) {
+      const id = req.body.id;
+      Question.remove({_id: id}, function(err, q) {
+        if(err) {
+          res.send(err);
+        }
+        res.json({message: 'Question deleted'});
+      });
+    });
+
+    router.route('/questions/update')
+    .post(function(req, res) {
+      Question.findById(req.body.id, function(err, q) {
+        if(err) {
+          res.send(err);
+        }
+        q.answer = req.body.answer;
+        q.value = req.body.value;
+        q.question = req.body.question;
+        q.categoryName = req.body.categoryName;
+
+        q.save(function(err) {
+          if(err) {
+            res.send(err);
+          }
+          res.json({message: 'Question updated!'});
+        })
+      })
+
     });
 
     router.route('/questions/random').get(function(req, res) {
-      // const rand = Math.random();
       Question.aggregate([{$sample: {size: 1}}], function(err, q) {
         if(err) {
           res.send(err);
@@ -66,12 +97,12 @@
 
     router.route('/questions/:question_id')
     .get(function(req, res) {
-        Question.findById(req.params.question_id, function(err, q) {
-            if(err) {
-                res.send(err);
-            }
-            res.json(q);
-        });
+      Question.findById(req.params.question_id, function(err, q) {
+        if(err) {
+          res.send(err);
+        }
+        res.json(q);
+      });
     });
 
     // Register routes
@@ -79,6 +110,6 @@
     app.use('/api', router);
 
     app.listen(port, function() {
-        console.log('Quiz API started');
+      console.log('Quiz API started');
     });
 }());
